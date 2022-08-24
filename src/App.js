cdadfde8fe24a3d70ce398115ca9eb0e7bc4af71
IFSul <marco.pereiradoespiritosanto@gmail.com>
2022-08-24 12:00:42 -0300

    2.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState , useEffect} from 'react'
-import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill, BsFillBookmarkCheckFill } from 'react-icons/bs'
 
 
 const API = "http://localhost:5000";
@@ -12,13 +12,108 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
+
+useEffect(() => {
+  const loadData = async () => {
+    setLoading(true);
+    const res = await fetch(API+"/todos")
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => console.log(error));
+
+    setLoading(false);
+
+    setTodos(res);
+  };
+
+  loadData();
+},[]);
+
+
+
+const handleSubmit = async(e)=> {
+  e.preventDefault();
+  
+  const todo = {
+    id: Math.random(),
+    title,
+    time,
+    done: false,
+  };
+
+await fetch(API + "/todos", {
+  method: "POST",
+  body: JSON.stringify(todo),
+  headers: { 'Content-Type': 'application/json' },
+
+});
+
+setTodos((prevState)=> [...prevState, todo]);
+  setTitle("");
+  setTime("");
+};
+
+  const handleDelete = async (id)=> {
+      
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+    setTodos((prevState)=> prevState.filter((todo)=>todo.id !== id));
+  };
+
+  const handleEdit = async (todo)=> {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+    headers: { 'Content-Type': 'application/json' },
+
+    });
+    setTodos((prevState)=> prevState.map((t)=>(t.id === data.id ? (t = data) : t)));
+  };
+
+
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
+
   return (
     <div className="App">
         <div className='todo-header'>
           <h1>React Todo</h1>
         </div>
-        <div>
-          div.todo-head
+        <div className='form-todo'>
+          <h2>Insira a sua proxima tarefa:</h2>
+          <form onSubmit={handleSubmit}>
+            <div className='form-control'>
+                <label htmlFor='title'> O que voce vai fazer? </label>
+                <input type='text' name='title' placeholder='Titulo da Tarefas' onChange={(e)=> setTitle(e.target.value)} value={title || ""} required/>
+            </div>
+            <div className='form-control'>
+                <label htmlFor='time'> Duração: </label>
+                <input type='text' name='time' placeholder='Tempo estimado (em horas)' onChange={(e)=> setTime(e.target.value)} value={time || ""} required/>
+            </div>
+            <input type='submit' value='Criar Tarefa' />
+          </form>
+        </div>
+        <div className='list-todo'>
+          <h2>Lista de Tarefas:</h2>
+          {todos.length === 0 && <p>Não há Tarefas!</p>}
+          {todos.map((todo) => (
+            <div className='todo' key={todo.id}> 
+             <h3 className={todo.done ? 'todo-done' : ''}>{todo.title}</h3>
+             <p >Duração: {todo.time}</p>
+             <div className='actions'>
+              <span onClick={() =>handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsFillBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)}/>
+              </div>
+            </div>
+          ))}
         </div>
     </div>
   );
